93c30820d62f03cb825d1af04132d11c6ef57e31
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2023-09-08 17:14:17 +0300

    change next.config

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import TodoList from "@/components/TodoList";
 
 export default function Home() {
-    // return <TodoList />;
-    return <div>Here must be TodoList</div>;
+    return <TodoList />;
+    // return <div>Here must be TodoList</div>;
 }
