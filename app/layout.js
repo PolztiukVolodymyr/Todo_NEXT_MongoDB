edcd9bd80aafc36d10a4a0a92b35c3d4ba048669
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2023-09-10 23:56:59 +0300

    eb in layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-// import { ErrorBoundary } from "react-error-boundary";
+import { ErrorBoundary } from "react-error-boundary";
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -15,10 +15,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="max-w-3xl mx-auto p-4">
-          {/* <ErrorBoundary fallback={<div>Something went wrong in Layout</div>}> */}
           <Navbar />
-          <div className="mt-8">{children}</div>
-          {/* </ErrorBoundary> */}
+          <ErrorBoundary fallback={<div>Something went wrong in Layout</div>}>
+            <div className="mt-8">{children}</div>
+          </ErrorBoundary>
         </div>
       </body>
     </html>
