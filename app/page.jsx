4ef6bb7968e5270224c61c30af59cf6a7c74999d
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2023-09-11 20:15:51 +0300

    add todoList

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,7 @@ import TodoList from "@/components/TodoList";
 export default function Home() {
     return (
         <ErrorBoundary fallback={<div>Something went wrong in TodoList</div>}>
-            {/* <TodoList /> */}
-            <div>Here must be TodoList</div>
+            <TodoList />
         </ErrorBoundary>
     );
 
